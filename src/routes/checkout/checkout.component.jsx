123a7df1358cb6ed88cc0a829d9cadd928b189bf
Bruce Jacobo <bruceja18@protonmail.com>
2023-11-06 13:22:21 -0800

    Created Checkout page with cart func.

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -2,26 +2,34 @@ import { useContext } from "react";
 import { CartContext } from "../../contexts/cart.context";
 
 import "./checkout.styles.scss";
+import CheckOutItem from "../../components/checkout-item/checkout-item.component";
 
 const CheckOut = () => {
-  const { cartItems } = useContext(CartContext);
+  const { cartItems, cartTotal} =
+    useContext(CartContext);
 
   return (
-    <div>
-      <h1>I am the one</h1>
-
-      <div>
-        {cartItems.map((cartItem) => {
-          const { name, quantity, id } = cartItem;
-
-          return (
-            <div key={id}>
-              <h2>{name}</h2>
-              <span>{quantity}</span>
-            </div>
-          );
-        })}
+    <div className="checkout-container">
+      <div className="checkout-header">
+        <div className="header-block">
+          <span>Product</span>
+        </div>
+        <div className="header-block">
+          <span>Description</span>
+        </div>
+        <div className="header-block">
+          <span>Quantity</span>
+        </div>
+        <div className="header-block">
+          <span>Price</span>
+        </div>
+        <div className="header-block">
+          <span>Remove</span>
+        </div>
       </div>
+
+      {cartItems.map((cartItem) => <CheckOutItem key={cartItem.id} cartItem={cartItem} />)}
+      <span className="total">Total: ${cartTotal}</span>
     </div>
   );
 };
