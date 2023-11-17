ac5c971a8e6b4b775fb7bae082707806126e5249
Bruce Jacobo <bruceja18@protonmail.com>
2023-11-16 16:13:13 -0800

    Add product images.

---
+++ src/shop-data.js
@@ -0,0 +1,60 @@
+  const SHOP_DATA = [
+    {
+      title: 'Crystals',
+      items: [
+        {
+          id: 5,
+          name: "Obsidian Pyramid",
+          imageUrl: "assets/images/shop-images/image_5.jpg" ,
+          price: 25
+        }
+      ],
+    },
+    {
+      title: 'Books',
+      items: [
+        {
+          id: 3,
+          name: "Occult Book",
+          imageUrl: "assets/images/shop-images/image_3.jpg" ,
+          price: 35
+        }
+      ],
+    },
+    {
+      title: 'Artifacts',
+      items: [
+        {
+          id: 4,
+          name: "Sphinx Statue",
+          imageUrl: "assets/images/shop-images/image_4.jpg" ,
+          price: 25
+        }
+      ],
+    },
+    {
+      title: 'Womens',
+      items: [
+        {
+          id: 6,
+          name: 'Wicca Dress',
+          imageUrl: 'assets/images/category-pages/women/wicca_dress.jpg',
+          price: 25,
+        }
+      ],
+    },
+    {
+      title: 'Mens',
+      items: [
+        {
+          id: 1,
+          name: "Plague Doctor Mask",
+          imageUrl: "assets/images/shop-images/image_1.jpg" ,
+          "price": 25 
+        }
+      ],
+    },
+  ];
+  
+  
+  export default SHOP_DATA;
